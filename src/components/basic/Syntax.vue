c69731087056690bad5390d5a69ac057cc24be8e
Jing yk <773669388@qq.com>
2024-07-23 22:45:26 +0800

    feat: 条件渲染

--- src/components/basic/Syntax.vue
+++ src/components/basic/Syntax.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
 
-    <!-- v-if -->
-    <h1 v-if="useVif"> hahaJYK </h1>
-    <h1 v-else> JYKnoHaha </h1>
-
-    <button @click="changeHaha">DjySB</button>
+    <!-- v-if 条件渲染 -->
+    <h1> v-if 条件渲染</h1>
+    <h3 v-if="useVif"> JYKxixi </h3>
+    <h3 v-else> JYKnoXixi </h3>
+    <button @click="changeHaha">change v-if</button>
     <!-- <h1> hahaJYK </h1> -->
+
+    <!-- 连续判断 -->
+    <h3 v-if="ifType==='A'"> JYK-type-A </h3>
+    <h3 v-else-if="ifType==='B'"> JYK-type-B </h3>
+    <h3 v-else-if="ifType==='C'"> JYK-type-C </h3>
+    <h3 v-else> JYK-type-Not A/B/C </h3>
+    <el-input v-model="ifType" placeholder="请输入A或B或C或其他"></el-input>
+
     
   </div>
 </template>
@@ -17,10 +25,14 @@ export default {
   name: 'Syntax',
   data() {
     return{
-      useVif: true
+      useVif: true,   // v-if是否显示
+      ifType: ''     // v-if连续判断
     }
   },
   methods: {
+    /**
+     * 改变v-if的值
+     */
     changeHaha() {
       this.useVif =! this.useVif;
     }
