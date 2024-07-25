27bde4ddd53409fddf4120571dc14c915df2c368
Jing yk <773669388@qq.com>
2024-07-25 23:27:34 +0800

    feat:引入Axios 30%

--- info/framework.md
+++ info/framework.md
@@ -1,6 +1,13 @@
 Vue
 
-Axios
+### Axios
+是一个开源的可以用在浏览器端和NodeJS的异步通信框架
+主要功能是实现Ajax通信
+因为Vue只关注视图层，所以通信推荐使用Axios
+
+使用Axios：
+npm install axios
+
 
 ### ElementUI
 参考地址：
---
+++ src/components/basic/Axios.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+
+    <!-- Axios -->
+    <h1> Axios</h1>
+    {{ message3 }}
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'List',
+  data() {
+    return{
+        
+    }
+  },
+  methods: {
+    /**
+     * 点击按钮1
+     */
+    clickButton1() {
+        alert("点击了按钮1");
+    },
+
+    
+
+  }
+}
+</script>
