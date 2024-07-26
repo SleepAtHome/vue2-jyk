0086ae8f55a9beedbf82f593434762aa89a75352
Jing yk <773669388@qq.com>
2024-07-27 03:28:11 +0800

    feat: 创建主页，使用路由跳转

---
+++ src/components/basic/JYKIndex.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1> 景元奎的主页 </h1>
+
+    菜单页：
+    <el-button type="success" @click="jump2FoodMenu" round>Food Menu</el-button>
+    <br>
+
+    语法等：
+    <!-- 路由的第2种方式：router-link -->
+    <router-link to="/syntax/if"> v-if 用法</router-link>
+    <br>
+    <el-button type="primary" @click="jump2AnyPage('If')" round>v-if 用法</el-button>
+    <el-button type="info" @click="jump2AnyPage('List')" round>v-for 用法</el-button>
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'JYKIndex',
+  data() {
+    return{
+    }
+  },
+  methods: {
+    /**
+     * 路由的第一种方式：this.$router.push
+     */
+    jump2FoodMenu() {
+      this.$router.push({name: 'foodMenu'});
+    },
+
+    /**
+     * 跳转去任何页面：入参: 页面name
+     */
+    jump2AnyPage(val) {
+      this.$router.push({name: val});
+    }
+  }
+}
+</script>
---
+++ src/components/basic/syntax/VModel.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+
+    <!-- v-model 表单输入绑定 -->
+    <h1> v-model 表单输入绑定</h1>
+
+    <p>Message is: {{ message }}</p>
+    <input v-model="message" placeholder="edit me" />
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'VModel',
+  data() {
+    return{
+        message: '',
+    }
+  },
+  methods: {
+    /**
+     * 点击按钮1
+     */
+    clickButton1() {
+        alert("点击了按钮1");
+    },
+
+  }
+}
+</script>
