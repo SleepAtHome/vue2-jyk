b927b6c3ad2c8d9f4d61246acc97cce7c36a872c
Jing yk <773669388@qq.com>
2024-08-23 01:04:28 +0800

    feat: 获取每日清单数据

--- src/components/checklist/Checklist.vue
+++ src/components/checklist/Checklist.vue
@@ -2,6 +2,8 @@
   <div>
     <h1>每日清单</h1>
     {{ this.$store.state.userInfo }}
+    {{ allCheckList }}
+    {{ userTodayThing }}
     <el-table
       :data="checklistData"
       style="width: 100%"
@@ -20,7 +22,9 @@ import axios from "axios";
 export default {
   name: "checklist",
   created() {},
-  mounted() {},
+  mounted() {
+    this.getAllCheckListMapper();
+  },
   data() {
     return {
       checklistData: [
@@ -45,6 +49,9 @@ export default {
           address: "上海市普陀区金沙江路 1518 弄",
         },
       ],
+
+      allCheckList: [],
+      userTodayThing: [],
     };
   },
   methods: {
@@ -52,12 +59,18 @@ export default {
      * 获取所有事项清单
      */
     getAllCheckListMapper() {
+      let _this = this;
+
       let getAllCheckListParam = {
-        userId: this.loginForm.account,
+        userId: this.$store.state.userInfo.userId,
       };
 
+      // 获取当前用户事项列表
       axios
-        .post("http://localhost:8080/jyk-total/check-list/search-condition", getAllCheckListParam)
+        .post(
+          "http://localhost:8080/jyk-total/check-list/search-condition",
+          getAllCheckListParam
+        )
         .then((response) => {
           if (response.data.code == "S000") {
             _this.$message({
@@ -65,9 +78,41 @@ export default {
               type: "success",
               showClose: true,
             });
+            _this.allCheckList = response.data.data;
+          } else {
+            _this.$message({
+              message: "登录失败",
+              type: "error",
+              showClose: true,
+            });
+          }
+        })
+        .catch((error) => {
+          _this.$message({
+            message: "登录异常" + error,
+            type: "error",
+            showClose: true,
+          });
+        });
 
-            // 路由跳转
-            _this.$router.push("/jykIndex");
+      // 获取当前用户当天事项清单
+
+      let getUserTodatThingParam = {
+        userId: this.$store.state.userInfo.userId,
+      };
+      axios
+        .post(
+          "http://localhost:8080/jyk-total/check-list-record/search-today-condition",
+          getUserTodatThingParam
+        )
+        .then((response) => {
+          if (response.data.code == "S000") {
+            _this.$message({
+              message: "登录成功",
+              type: "success",
+              showClose: true,
+            });
+            _this.userTodayThing = response.data.data;
           } else {
             _this.$message({
               message: "登录失败",
