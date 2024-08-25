229a7e9520853426e462721a98bd4a59c7941c52
Jing yk <773669388@qq.com>
2024-08-26 00:46:02 +0800

    perf: 优化sessionStorage存取

--- src/components/checklist/MyMatterPage.vue
+++ src/components/checklist/MyMatterPage.vue
@@ -4,6 +4,32 @@
         我的每日事项
         <el-button type="primary" @click="addCheckListDay">增加事项</el-button>
     </h1>
+
+    <el-dialog
+        title="增加事项"
+        :visible.sync="addMatterDialogVisible"
+        width="30%">
+
+        <el-form :model="addMatterDialogForm">
+            <el-form-item label="事项名称">
+                <el-input v-model="addMatterDialogForm.matter" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="频率">
+                <el-select v-model="addMatterDialogForm.frequency" placeholder="请选择事项每日频率">
+                    <el-option label="1" value=1></el-option>
+                    <el-option label="2" value=2></el-option>
+                    <el-option label="3" value=3></el-option>
+                    <el-option label="4" value=4></el-option>
+                    <el-option label="5" value=5></el-option>
+                </el-select>
+            </el-form-item>
+        </el-form>
+
+        <span slot="footer" class="dialog-footer">
+            <el-button @click="addMatterDialogVisible = false">取消</el-button>
+            <el-button type="primary" @click="submitAddMatter">提交</el-button>
+        </span>
+    </el-dialog>
     
     
     <el-table
@@ -29,16 +55,33 @@ import axios from "axios";
 
 export default {
   name: "myMatterPage",
-  created() {},
+  created() {
+    this.checkLoginStatus();
+  },
   mounted() {
     this.getMyDayChecklistInfo();
   },
   data() {
     return {
       myDayChecklist: [],    //  最终聚合结果
+      addMatterDialogVisible: false, //  增加事项对话框是否显示
+      addMatterDialogForm: {},  //  增加事项对话框表单数据
     };
   },
   methods: {
+    checkLoginStatus() {
+      //  检查是否登录
+      console.log(JSON.parse(sessionStorage.getItem('onlineUserKey')).userId);
+      if (JSON.parse(sessionStorage.getItem('onlineUserKey')).userId == null) {
+        this.$message({
+          message: "请先登录",
+          type: "error",
+          showClose: true,
+        });
+        this.$router.push('LoginPage');
+      }
+      
+    },
     /**
      * 获取事项清单
      */
@@ -46,7 +89,7 @@ export default {
       let _this = this;
       let myDayChecklistInfoParam = {
         // 当前用户Id
-        userId: sessionStorage.getItem('onlineUserKey').userId,
+        userId: JSON.parse(sessionStorage.getItem('onlineUserKey')).userId,
       };
       // 获取当前用户事项列表
       axios.post("http://localhost:8080/jyk-total/check-list/search-condition",myDayChecklistInfoParam)
@@ -83,7 +126,45 @@ export default {
     },
 
     addCheckListDay () {
+        // 清空表单
+        this.addMatterDialogForm = {};
 
+        this.addMatterDialogVisible = true;
+    },
+
+    submitAddMatter () {
+        let _this = this;
+        let addMatterParam = {
+            userId: JSON.parse(sessionStorage.getItem('onlineUserKey')).userId,
+            matter: this.addMatterDialogForm.matter,
+            frequency: this.addMatterDialogForm.frequency,
+        };
+
+        axios.post("http://localhost:8080/jyk-total/check-list/add",addMatterParam).then((response) => {
+            if (response.data.code == "S000") {
+            _this.$message({
+                message: "增加事项成功",
+                type: "success",
+                showClose: true,
+            });
+            } else {
+            _this.$message({
+                message: "增加事项失败",
+                type: "error",
+                showClose: true,
+            });
+            }
+        })
+        .catch((error) => {
+            _this.$message({
+            message: "增加事项异常" + error,
+            type: "error",
+            showClose: true,
+            });
+        });
+
+        // 关闭对话框
+        this.addMatterDialogVisible = false;    
     },
 
     /**
